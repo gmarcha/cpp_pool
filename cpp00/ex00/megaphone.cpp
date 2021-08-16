0b9a14e430f65e0a1ac1586dd4f678ddf59abcf7
gmarcha <gmarcha@github.com>
2021-08-16 15:21:10 +0200

    update cpp00: add ex00

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,16 @@
+#include "iostream"
+
+int	main(int ac, char *av[]) {
+
+	if (ac < 2) {
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	for (int i = 1; i < ac; i++) {
+		for (int j = 0; av[i][j] != '\0'; j++) {
+			std::cout << (char)std::toupper(av[i][j]);
+		}
+	}
+	std::cout << std::endl;
+	return (0);
+}
