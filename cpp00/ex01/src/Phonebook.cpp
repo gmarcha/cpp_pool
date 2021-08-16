d071454df5e0af2b9dd49a17669f1edabab2ee01
gmarcha <gmarcha@github.com>
2021-08-16 16:46:50 +0200

    update cpp00: update circular buffer to ex01

--- cpp00/ex01/src/Phonebook.cpp
+++ cpp00/ex01/src/Phonebook.cpp
@@ -22,30 +22,35 @@ void		Phonebook::add() {
 		_nb_contacts++;
 	}
 
-	std::cout << "Enter first name: ";
+	std::cout << FG_CYAN"Enter first name: "RESET;
 	std::cin >> input;
 	_contacts[index].set_first_name(input);
 
-	std::cout << "Enter last name: ";
+	std::cout << FG_CYAN"Enter last name: "RESET;
 	std::cin >> input;
 	_contacts[index].set_last_name(input);
 
-	std::cout << "Enter nickname: ";
+	std::cout << FG_CYAN"Enter nickname: "RESET;
 	std::cin >> input;
 	_contacts[index].set_nick_name(input);
 
-	std::cout << "Enter phone number: ";
+	std::cout << FG_CYAN"Enter phone number: "RESET;
 	std::cin >> input;
 	_contacts[index].set_phone_number(input);
 
-	std::cout << "Enter darkest secret: ";
+	std::cout << FG_CYAN"Enter darkest secret: "RESET;
 	std::cin >> input;
 	_contacts[index].set_darkest_secret(input);
 
 }
 
 void		Phonebook::search(const size_t input_index) const {
-	search_infos(_contacts[input_index - 1]);
+	size_t			index = (input_index - 1) + _start_contact % 8;
+
+	if (index >= 8)
+		search_infos(_contacts[index - 8]);
+	else
+		search_infos(_contacts[index]);
 }
 
 void		Phonebook::search_infos(const Contact &contact) const {
@@ -58,7 +63,7 @@ void		Phonebook::search_infos(const Contact &contact) const {
 void		Phonebook::print() const {
 	size_t			index = 1;
 
-	for (size_t i = _start_contact; i < _nb_contacts; i++) {
+	for (size_t i = _start_contact % 8; i < _nb_contacts; i++) {
 		std::cout << "         " << index;
 		print_contacts(_contacts[i].get_first_name());
 		print_contacts(_contacts[i].get_last_name());
@@ -66,7 +71,7 @@ void		Phonebook::print() const {
 		std::cout << std::endl;
 		index++;
 	}
-	for (size_t i = 0; i < _start_contact; i++) {
+	for (size_t i = 0; i < _start_contact % 8; i++) {
 		std::cout << "         " << index;
 		print_contacts(_contacts[i].get_first_name());
 		print_contacts(_contacts[i].get_last_name());
