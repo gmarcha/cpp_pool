c3818742dc681a06261b27672e7ff646fba7522b
gmarcha <gmarcha@github.com>
2021-08-23 20:29:28 +0200

    update src: add humanA and humanB class declaration

--- cpp01/ex03/HumanB.cpp
+++ cpp01/ex03/HumanB.cpp
@@ -1,2 +1,18 @@
 #include "HumanB.hpp"
 
+HumanB::HumanB(const std::string &name) : _name(name), _weapon(0) {
+
+}
+
+HumanB::~HumanB() {
+
+}
+
+void				HumanB::setWeapon(Weapon &weapon) {
+	this->_weapon = &weapon;
+}
+
+void				HumanB::attack() const {
+	std::cout << this->_name <<  " attacks with his " << this->_weapon->getType() << std::endl;
+}
+
