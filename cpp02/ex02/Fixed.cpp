d4ca34fbfffa5cfc6c23dac23973a9b4437abdb6
gmarcha <gmarcha@github.com>
2021-08-24 20:47:09 +0200

    update fixed: add overload operators and overload functions to fixed class

---
+++ cpp02/ex02/Fixed.cpp
@@ -0,0 +1,138 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed(void) : _rawBits(0) {}
+
+Fixed::Fixed(const int nb) : _rawBits(nb << _nbFractionalBits) {}
+
+Fixed::Fixed(const float nb) : _rawBits(roundf(nb * (1 << _nbFractionalBits))) {}
+
+Fixed::Fixed(const Fixed &src) : _rawBits(src._rawBits) {}
+
+Fixed::~Fixed() {}
+
+Fixed				&Fixed::operator=(const Fixed &rhs) {
+
+	if (this != &rhs) {
+		this->_rawBits = rhs._rawBits;
+	}
+	return (*this);
+}
+
+Fixed&				Fixed::operator++(void) {
+
+	this->_rawBits++;
+	return (*this);
+}
+
+Fixed&				Fixed::operator--(void) {
+
+	this->_rawBits--;
+	return (*this);
+}
+
+Fixed				Fixed::operator++(int) {
+
+	Fixed			old(*this);
+	operator++();
+	return (old);
+}
+
+Fixed				Fixed::operator--(int) {
+
+	Fixed			old(*this);
+	operator--();
+	return (old);
+}
+
+Fixed				Fixed::operator+(const Fixed &rhs) const {
+	return (Fixed(this->toFloat() + rhs.toFloat()));
+}
+
+Fixed				Fixed::operator-(const Fixed &rhs) const {
+	return (Fixed(this->toFloat() - rhs.toFloat()));
+}
+
+Fixed				Fixed::operator*(const Fixed &rhs) const {
+	return (Fixed(this->toFloat() * rhs.toFloat()));
+}
+
+Fixed				Fixed::operator/(const Fixed &rhs) const {
+	return (Fixed(this->toFloat() / rhs.toFloat()));
+}
+
+bool				Fixed::operator==(const Fixed &rhs) const {
+	return (this->_rawBits == rhs._rawBits);
+}
+
+bool				Fixed::operator!=(const Fixed &rhs) const {
+	return (!(*this == rhs));
+}
+
+bool				Fixed::operator<(const Fixed &rhs) const {
+	return (this->_rawBits < rhs._rawBits);
+}
+
+bool				Fixed::operator>(const Fixed &rhs) const {
+	return (rhs < *this);
+}
+
+bool				Fixed::operator<=(const Fixed &rhs) const {
+	return (!(*this > rhs));
+}
+
+bool				Fixed::operator>=(const Fixed &rhs) const {
+	return (!(*this < rhs));
+}
+
+int					Fixed::getRawBits(void) const {
+	return (this->_rawBits);
+}
+
+void				Fixed::setRawBits(const int raw) {
+	this->_rawBits = raw;
+}
+
+float				Fixed::toFloat(void) const {
+	return (this->_rawBits / static_cast<float>(1 << _nbFractionalBits));
+}
+
+int					Fixed::toInt(void) const {
+	return (this->_rawBits >> _nbFractionalBits);
+}
+
+Fixed&				Fixed::min(Fixed &a, Fixed &b) {
+	
+	if (a < b)
+		return (a);
+	return (b);
+}
+
+Fixed&				Fixed::max(Fixed &a, Fixed &b) {
+
+	if (a < b)
+		return (b);
+	return (a);
+}
+
+const Fixed&		Fixed::min(const Fixed &a, const Fixed &b) {
+
+	// std::cout << "const argument" << std::endl;
+	if (a < b)
+		return (a);
+	return (b);
+}
+
+const Fixed&		Fixed::max(const Fixed &a, const Fixed &b) {
+
+	// std::cout << "const argument" << std::endl;
+	if (a < b)
+		return (b);
+	return (a);
+}
+
+std::ostream&		operator<<(std::ostream &out, const Fixed &rhs) {
+	out << rhs.toFloat();
+	return (out);
+}
+
+const int			Fixed::_nbFractionalBits = 8;
---
+++ cpp02/ex02/main.cpp
@@ -0,0 +1,45 @@
+#include "Fixed.hpp"
+
+int	main(void) {
+
+	Fixed 		x;
+	Fixed		y(42);
+	const Fixed add(Fixed(10) + Fixed(12) + Fixed(20));
+	const Fixed sub(Fixed(142) - Fixed(42) - Fixed(58.0f));
+	const Fixed mul(Fixed(42) * Fixed(0.25f) * Fixed(2));
+	const Fixed div(Fixed(105) / Fixed(2) / Fixed(2.5f));
+
+	std::cout << "x:            " << x << std::endl;
+	std::cout << "++x:          " << ++x << std::endl;
+	std::cout << "x:            " << x << std::endl;
+	std::cout << "x++:          " << x++ << std::endl;
+	std::cout << "x:            " << x << std::endl << std::endl;
+
+	std::cout << "add:          " << add << std::endl;
+	std::cout << "sub:          " << sub << std::endl;
+	std::cout << "mul:          " << mul << std::endl;
+	std::cout << "div:          " << div << std::endl << std::endl;
+
+	std::cout << "add == sub:   " << std::boolalpha << (add == sub) << std::endl;
+	std::cout << "add == x:     " << std::boolalpha << (add == x) << std::endl;
+	std::cout << "add != sub:   " << std::boolalpha << (add != sub) << std::endl;
+	std::cout << "add != x:     " << std::boolalpha << (add != x) << std::endl << std::endl;
+
+	std::cout << "add < sub:    " << std::boolalpha << (add < sub) << std::endl;
+	std::cout << "add < x:      " << std::boolalpha << (add < x) << std::endl;
+	std::cout << "add > sub:    " << std::boolalpha << (add > sub) << std::endl;
+	std::cout << "add > x:      " << std::boolalpha << (add > x) << std::endl << std::endl;
+
+	std::cout << "add <= sub:   " << std::boolalpha << (add <= sub) << std::endl;
+	std::cout << "add <= x:     " << std::boolalpha << (add <= x) << std::endl;
+	std::cout << "add >= sub:   " << std::boolalpha << (add >= sub) << std::endl;
+	std::cout << "add >= x:     " << std::boolalpha << (add >= x) << std::endl << std::endl;
+
+	std::cout << "max(add,mul): " << Fixed::max(add,mul) << std::endl;
+	std::cout << "min(add,mul): " << Fixed::min(add,mul) << std::endl;
+
+	std::cout << "max(x,y):     " << Fixed::max(x,y) << std::endl;
+	std::cout << "min(x,y):     " << Fixed::min(x,y) << std::endl;
+
+	return 0;
+}
