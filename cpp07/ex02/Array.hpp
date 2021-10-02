366bd7ffcc759f6b71ceae45635518ee7587478c
gmarcha <gmarcha@github.com>
2021-10-02 17:32:10 -0400

    update cpp07: add ex02

---
+++ cpp07/ex02/Array.hpp
@@ -0,0 +1,68 @@
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+# include <stddef.h>
+
+template<typename T>
+class Array {
+
+    public:
+
+        // Constructors and destructor
+        Array(void) :
+            _array(new T()), _size(0) {}
+        Array(const unsigned int n) :
+            _array(new T[n]()), _size(n) {}
+        Array(const Array &src) :
+            _array(new T[src._size]), _size(src._size) {
+            for (size_t i = 0; i < src._size; i++) {
+                _array[i] = src._array[i];
+            }
+        }
+        virtual ~Array() {
+            if (_array != NULL) {
+                delete[] _array;
+            }
+        }
+
+        // Operator overloads
+        Array&	operator=(const Array &rhs) {
+            _size = rhs._size;
+            if (_array != NULL) {
+                delete[] _array;
+            }
+            _array = new T[_size]();
+            for (size_t i = 0; i < _size; i++) {
+                _array[i] = rhs._array[i];
+            }
+            return *this;
+        }
+
+        T&      operator[](const unsigned int i) {
+            if (_array == NULL || _size <= i) {
+                throw Array::ArrayOutOfBounds();
+            }
+            return _array[i];
+        }
+
+        // Member functions
+        unsigned int    size(void) const {
+            return _size;
+        }
+
+        class ArrayOutOfBounds : public std::exception {
+            public:
+                virtual const char* what() const throw() {
+                    return "Out of bounds access to array";
+                }
+        };
+
+    private:
+
+        // Attributes
+        T*                  _array;
+        unsigned int        _size;
+
+};
+
+#endif // ARRAY_HPP
---
+++ cpp07/ex02/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include "Array.hpp"
+
+int main(void) {
+
+    Array<int>   intArray(10);
+
+    std::cout << "size: " << intArray.size() << std::endl;
+    std::cout << intArray[0] << std::endl;
+
+    try {
+        std::cout << intArray[10] << std::endl;
+    } catch(std::exception &e) {
+        std::cout << "operation failed: " << e.what() << std::endl;
+    }
+
+    std::cout << std::endl;
+
+    Array<std::string>  strArray(2);
+
+    std::cout << "size: " << strArray.size() << std::endl;
+    std::cout << "[" << strArray[0] << "]" << std::endl;
+
+    strArray[0] = "Marvin the paranoid Android";
+    strArray[1] = "Deep Thought";
+
+    std::cout << strArray[0] << ", " << strArray[1] << std::endl;
+
+    try {
+        std::cout << strArray[2] << std::endl;
+    } catch(std::exception &e) {
+        std::cout << "operation failed: " << e.what() << std::endl;
+    }
+
+    std::cout << std::endl;
+
+    Array<std::string>  strArrayCopy(strArray);
+
+    std::cout << "size: " << strArrayCopy.size() << std::endl;
+    std::cout << strArrayCopy[0] << std::endl;
+    strArrayCopy[0] = "Marvin";
+    std::cout << strArrayCopy[0] << std::endl;
+    std::cout << strArray[0] << " (original array)" << std::endl;
+
+    std::cout << std::endl;
+
+    strArrayCopy = strArray;
+    std::cout << strArrayCopy[0] << std::endl;
+
+    return 0;
+}
