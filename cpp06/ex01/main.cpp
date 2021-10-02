dc4235da741a1ff01725f95642a561647b55cb4b
gmarcha <gmarcha@github.com>
2021-10-02 14:54:34 -0400

    update cpp06: update ex01

--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -4,18 +4,43 @@
 
 int main(void) {
 
-    Data* dataPtr = new Data("11001010");
+    std::cout << "======= WORKING TEST ==" << std::endl;
 
-    uintptr_t code = serialize(dataPtr);
-    Data* decode = deserialize(code);
+    {
+        Data* dataPtr = new Data("11001010");
 
-    const bool areCodesMatching = (dataPtr == decode);
+        uintptr_t code = serialize(dataPtr);
+        Data* decode = deserialize(code);
 
-    std::cout << "Serialize and deserialize are ";
-    if (areCodesMatching == false) {
-        std::cout << "not ";
+        const bool areCodesMatching = (dataPtr == decode);
+
+        std::cout << "Serialize and deserialize are ";
+        if (areCodesMatching == false) {
+            std::cout << "not ";
+        }
+        std::cout << "working correctly" << std::endl;
+
+        delete dataPtr;
     }
-    std::cout << "working correctly" << std::endl;
+
+    std::cout << std::endl << "======= ERROR TEST ====" << std::endl;
+
+    {
+        Data* dataPtr = new Data("11001010");
+
+        uintptr_t code = serialize(dataPtr);
+        Data* decode = deserialize(code) - 1;
+
+        const bool areCodesMatching = (dataPtr == decode);
+
+        std::cout << "Serialize and deserialize are ";
+        if (areCodesMatching == false) {
+            std::cout << "not ";
+        }
+        std::cout << "working correctly" << std::endl;
+
+        delete dataPtr;
+    }
 
     return 0;
 }
