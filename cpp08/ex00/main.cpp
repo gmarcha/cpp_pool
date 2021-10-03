9efd8ccf28b77feb8b9b820e36087214e4b5c2df
gmarcha <gmarcha@github.com>
2021-10-02 20:14:58 -0400

    update cpp08: update ex00

--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <list>
 #include "easyfind.hpp"
 
 const char* EasyFindFail::what() const throw() {
@@ -10,6 +11,7 @@ int main(void) {
 
     int intArray[] = { 0, 42, -1 };
     size_t n = sizeof(intArray) / sizeof(intArray[0]);
+
     std::vector<int> intVector(intArray, intArray + n);
     
     try {
@@ -20,6 +22,22 @@ int main(void) {
 
     try {
         std::cout << easyFind(intVector, -42) << std::endl;
+    } catch(std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
+
+    std::cout << std::endl;
+
+    std::list<int> intList(intArray, intArray + n);
+    
+    try {
+        std::cout << easyFind(intList, 42) << std::endl;
+    } catch(std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
+
+    try {
+        std::cout << easyFind(intList, -42) << std::endl;
     } catch(std::exception &e) {
         std::cout << e.what() << std::endl;
     }
